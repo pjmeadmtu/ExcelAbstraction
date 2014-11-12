d10688806f0ce45af29ad5ecccf08a3ce1fd5ecf
Kelly Selden <kellyselden@gmail.com>
2014-11-11 19:09:22 -0500

    added tests

---
+++ ExcelAbstraction.Tests/ExcelCompare.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using ExcelAbstraction.Entities;
+using ExcelAbstraction.Services;
+
+namespace ExcelAbstraction.Tests
+{
+	public static class ExcelCompare
+	{
+		public static bool Compare(string fileName, params IExcelService[] excelServices)
+		{
+			return Compare(excelServices.Select(s => s.ReadWorkbook(fileName)).ToArray());
+		}
+		public static bool Compare(params Workbook[] workbooks)
+		{
+			for (int i = 0; i < workbooks.Length - 1; i++)
+			{
+				var worksheets1 = workbooks[i].Worksheets.ToArray();
+				var worksheets2 = workbooks[i + 1].Worksheets.ToArray();
+
+				if (worksheets1.Length != worksheets2.Length)
+					return false;
+
+				for (int j = 0; j < worksheets1.Length; j++)
+				{
+					var worksheet1 = worksheets1[j];
+					var worksheet2 = worksheets2[j];
+
+					if (worksheet1.Name != worksheet2.Name)
+						return false;
+
+					var rows1 = worksheet1.Rows.ToArray();
+					var rows2 = worksheet2.Rows.ToArray();
+
+					if (rows1.Length != rows2.Length)
+						return false;
+
+					for (int k = 0; k < rows1.Length; k++)
+					{
+						var row1 = rows1[k];
+						var row2 = rows2[k];
+
+						if (row1.Index != row2.Index)
+							return false;
+
+						var cells1 = row1.Cells.ToArray();
+						var cells2 = row2.Cells.ToArray();
+
+						if (cells1.Length != cells2.Length)
+							return false;
+
+						for (int l = 0; l < cells1.Length; l++)
+						{
+							var cell1 = cells1[l];
+							var cell2 = cells2[l];
+
+							if (cell1 == null && cell2 == null)
+								continue;
+							if (cell1 == null || cell2 == null)
+								return false;
+							if (cell1.RowIndex != cell2.RowIndex)
+								return false;
+							if (cell1.ColumnIndex != cell2.ColumnIndex)
+								return false;
+							if (cell1.Value != cell2.Value)
+								return false;
+						}
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ ExcelAbstraction.Tests/WorkbookTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using ExcelAbstraction.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelAbstraction.Tests
+{
+	[TestClass]
+	public class WorkbookTests
+	{
+		readonly DataSet _dataSet;
+
+		public WorkbookTests()
+		{
+			_dataSet = CreateDataSet();
+		}
+
+		[TestMethod]
+		public void Workbook_FromDataSet_WithoutHeader()
+		{
+			CompareDataSetAndWorkbook(false);
+		}
+
+		[TestMethod]
+		public void Workbook_FromDataSet_WithHeader()
+		{
+			CompareDataSetAndWorkbook(true);
+		}
+
+		void CompareDataSetAndWorkbook(bool header)
+		{
+			int modifier = header ? 1 : 0;
+			var workbook = Workbook.FromDataSet(_dataSet.Copy(), new CultureInfo("en-US"), header);
+			Assert.AreEqual(_dataSet.Tables.Count, workbook.Worksheets.Count());
+			foreach (Worksheet worksheet in workbook.Worksheets)
+			{
+				var dataTable = _dataSet.Tables[worksheet.Index];
+				Assert.AreEqual(dataTable.TableName, worksheet.Name);
+				Assert.AreEqual(dataTable.Columns.Count, worksheet.ColumnCount);
+				Assert.AreEqual(dataTable.Rows.Count + modifier, worksheet.Rows.Count());
+				if (header)
+					foreach (Cell cell in worksheet.Rows.First().Cells)
+						Assert.AreEqual(dataTable.Columns[cell.ColumnIndex].ColumnName, cell.Value);
+				foreach (Row row in worksheet.Rows.Skip(modifier))
+				{
+					var dataRow = dataTable.Rows[row.Index - modifier];
+					Assert.AreEqual(dataRow.ItemArray.Length, row.Cells.Count());
+					foreach (Cell cell in row.Cells)
+						Assert.AreEqual(dataRow[cell.ColumnIndex], cell.Value);
+				}
+			}
+		}
+
+		static DataSet CreateDataSet()
+		{
+			var dataSet = new DataSet();
+			for (int i = 0; i < 5; i++)
+			{
+				var dataTable = dataSet.Tables.Add(Guid.NewGuid().ToString().Substring(0, 31));
+				for (int j = 0; j < 4; j++)
+					dataTable.Columns.Add(Guid.NewGuid().ToString());
+				for (int j = 0; j < 6; j++)
+				{
+					var cells = new List<object>();
+					for (int k = 0; k < dataTable.Columns.Count; k++)
+						cells.Add(Guid.NewGuid());
+					dataTable.Rows.Add(cells.ToArray());
+				}
+			}
+			return dataSet;
+		}
+	}
+}
