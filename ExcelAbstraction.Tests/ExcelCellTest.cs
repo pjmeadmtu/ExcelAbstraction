b542eac5b7487d117f9cf6c7ef3eedd04dc6a596
Paul Mead <pmead@kpmg.com>
2017-04-20 09:24:40 -0400

    Allowing a data format for the cell to help handle dates, percentages, etc.
    Unit test for Excel Cell entity checking that DataFormat for expected inputs

---
+++ ExcelAbstraction.Tests/ExcelCellTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelAbstraction.Entities;
+
+namespace ExcelAbstraction.Tests
+{
+	[TestClass]
+	public class ExcelCellTest
+	{
+		[TestMethod]
+		public void TestCellDataFormat()
+		{
+			const string dateFormat = "MM/dd/yyyy";
+			var cell = new Cell(0, 2, "3/12/2017", dateFormat);
+
+			Assert.AreEqual(dateFormat, cell.DataFormat);
+		}
+
+		[TestMethod]
+		public void TestCellDataFormatEmpty()
+		{
+			var cell = new Cell(0, 2, "$40,000,000");
+			Assert.AreEqual(cell.DataFormat, string.Empty);
+		}
+	}
+}
--- ExcelAbstraction/Entities/Cell.cs
+++ ExcelAbstraction/Entities/Cell.cs
@@ -8,12 +8,14 @@ namespace ExcelAbstraction.Entities
 		public int RowIndex { get; private set; }
 		public int ColumnIndex { get; private set; }
 		public string Value { get; private set; }
+		public string DataFormat { get; private set; }
 
-		public Cell(int rowIndex, int columnIndex, string value)
+		public Cell(int rowIndex, int columnIndex, string value, string dataFormat = "")
 		{
 			RowIndex = rowIndex;
 			ColumnIndex = columnIndex;
 			Value = value;
+			DataFormat = dataFormat;
 		}
 	}
 }
